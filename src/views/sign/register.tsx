143d753e144145f30c38e97a0958311df302ac04
promise <365908865@qq.com>
2018-10-31 18:10:33 +0800

    注册

--- src/views/sign/register.tsx
+++ src/views/sign/register.tsx
@@ -8,9 +8,12 @@ import * as Fetch from '../../fetch';
 
 const FormItem = Form.Item;
 
+interface Params {
+    [name: string]: string
+}
 
 interface State {
-    params: object,
+    params: Params,
     msg: string,
     confirmDirty: boolean,
     autoCompleteResult: any[]
@@ -27,11 +30,53 @@ class RegistrationForm extends React.Component<FormComponentProps, any> {
         confirmDirty: false,
         autoCompleteResult: []
     }
+
+    inputList = [
+        {
+            name: 'userName',
+            type: 'user',
+            placeholder: '用户名',
+            rules: [
+                { required: true, message: '用户名不能为空' },
+                { type: 'string', min: 2, message: '用户名长度不能小于2位' },
+                { type: 'string', max: 12, message: '用户名长度不能大于12位' }
+            ]
+        },
+        {
+            name: 'loginName',
+            type: 'unlock',
+            placeholder: '账号',
+            rules: [
+                { required: true, message: '账号不能为空' },
+                { type: 'string', min: 2, message: '账号长度不能小于2位' },
+                { type: 'string', max: 12, message: '账号长度不能大于12位' }
+            ]
+        },
+        {
+            name: 'passWord',
+            type: 'lock',
+            placeholder: '密码',
+            rules: [
+                { required: true, message: '密码不能为空' },
+                { type: 'string', min: 6, message: '密码长度不能小于6位' },
+                { type: 'string', max: 24, message: '密码长度不能大于24位' }
+            ]
+        },
+        {
+            name: 'email',
+            type: 'mail',
+            placeholder: '邮箱',
+            rules: [
+                { required: true, message: '邮箱不能为空' },
+                { type: 'email', message: '邮箱格式错误' }
+            ]
+        },
+    ]
+
     handleSubmit = () => {
 
     }
-    model = (e?: any) => {
-        let key = e.target.attributes['data-model'].value;
+    model = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
         this.setState({
             params: {
                 ...this.state.params,
@@ -40,70 +85,50 @@ class RegistrationForm extends React.Component<FormComponentProps, any> {
         })
     }
     register = () => {
-        Fetch.post('register', this.state.params).then(data => {
-            this.setState({ msg: data.msg })
-        })
-    }
-    focus = (e: any) => {
-        console.log(e.target.type = 'password');
+        console.log(this.state.params)
+        // Fetch.post('register', this.state.params).then(data => {
+        //     this.setState({ msg: data.msg })
+        // })
     }
     render() {
-        const { model, register, focus } = this;
-        const { msg } = this.state;
+        const { model, register, inputList } = this;
+        const { msg, params } = this.state;
         const { getFieldDecorator } = this.props.form;
         return (
-            < Form layout="inline" className="register" onSubmit={this.handleSubmit}>
-                <p>
-                    emm 这是注册页面
-                </p>
+            <div className="register">
+                < Form layout="inline">
+                    <p>
+                        emm 这是注册页面
+                    </p>
 
-                <FormItem>
                     {
-                        getFieldDecorator('user', {
-                            rules: [
-                                { required: true, message: '用户名不能为空' },
-                                { type: 'string', min: 2, message: '用户名长度不能小于2位' },
-                                { type: 'string', max: 12, message: '用户名长度不能大于12位' }
-                            ]
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="用户名" onChange={model} data-model="userName" />
-                        )
+                        inputList.map(({ name, rules, placeholder, type }) => (
+                            <FormItem key={name} label={placeholder}>
+                                {
+                                    getFieldDecorator(name, { rules })(
+                                        <Input prefix={<Icon type={type} style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                            placeholder={placeholder} onChange={e => model(e, name)} />
+                                    )
+                                }
+                            </FormItem>
+                        ))
                     }
-                </FormItem>
-
-                <FormItem>
+                    <br />
+                    <FormItem>
+                        <Button type="primary">注册</Button>
+                    </FormItem>
                     {
-                        getFieldDecorator('lock', {
-                            rules: [
-                                { required: true, message: '账号不能为空' },
-                                { type: 'string', min: 2, message: '账号长度不能小于2位' },
-                                { type: 'string', max: 12, message: '账号长度不能大于12位' }
-                            ]
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type='text' onChange={model} placeholder="账号" data-model="loginName" />
-                        )
+                        msg &&
+                        <Alert
+                            message={msg}
+                            type="error"
+                            closable
+                        />
                     }
-                </FormItem>
 
+                </Form >
+            </div>
 
-
-                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type='text' data-autocomplete='off' onChange={model} placeholder="密码" data-model="passWord" />
-                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type='text' onChange={model} placeholder="邮箱" data-model="email" />
-                {
-                    msg &&
-                    <Alert
-                        message={msg}
-                        type="error"
-                        closable
-                    />
-                }
-                <Button type="primary">注册</Button>
-
-            </Form >
         )
     }
 }
